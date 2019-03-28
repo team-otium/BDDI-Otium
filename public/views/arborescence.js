f74e656b1f91c932ad210c4f30b7ce80b0454436
ESSOUBAI Nadia <nessoubai@g-a102b-prof.local>
2019-03-28 15:56:39 +0100

    set files questions

--- public/views/arborescence.js
+++ public/views/arborescence.js
@@ -1,4 +1,15 @@
 let intro = {
     animationLogo : new Page(animation_logo_desktop , animation_logo_mobile, "animation-logo"),
     intro : new Page(intro_desktop, intro_mobile, "intro"),
+    connexion : new Page(connexion_desktop, connexion_mobile, "connexion"),
+}
+
+let questions = {
+    q1 : new Page(q1_desktop, q1_mobile, "q1"),
+    q2 : new Page(q2_desktop, q2_mobile, "q2"),
+    q3 : new Page(q3_desktop, q3_mobile, "q3"),
+    q4 : new Page(q4_desktop, q4_mobile, "q4"),
+    q5 : new Page(q5_desktop, q5_mobile, "q5"),
+    q6 : new Page(q6_desktop, q6_mobile, "q6"),
+    q7 : new Page(q7_desktop, q7_mobile, "q7"),
 }
