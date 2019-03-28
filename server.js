5590f7f7e9c7404062d5297f285b01b1d0471bf4
RUCOJEVIC Rémi <rrucojevic@g-a102b-01.local>
2019-03-28 16:02:06 +0100

    UPDATE: server

--- server.js
+++ server.js
@@ -16,6 +16,48 @@ app.get('/', function(req, res) {
 
 app.use(express.static('public'))
 
+let io = require('socket.io').listen(server)
+
+io.sockets.on('connection', function (socket) {
+
+    socket.emit("getRoom", {data: computers })
+
+    socket.on('computerConnexion', function (id) {
+        socket.serverId = id.id
+        computers.push( socket.serverId.toString() )
+        socket.join(socket.serverId.toString())
+        socket.broadcast.emit('getRoom', {data: computers })
+    })
+
+    socket.on('askMobileConnexion', (code) => {
+        let i = computers.indexOf( code )
+        if (i > -1) {
+            if (io.sockets.adapter.rooms[code].length < 2) {
+                console.log(io.sockets.adapter.rooms[code].length)
+                socket.clientId = code
+                socket.join(code)
+                io.sockets.to(code).emit('mobileConnected')
+                console.log('connected')
+                /** TO DO : SEND MESSAGE TO ROOM TO START THE EXP */
+            } else {
+                /** TO DO : ERROR MESSAGE */
+            }
+        }
+    })
+
+    socket.on('disconnect', () => {
+        if (socket.serverId != undefined) { // Is computer
+            let i = computers.indexOf( socket.serverId.toString() )
+            if (i > -1) {
+                socket.broadcast.to(computers[i]).emit('computerDisconnected')
+                delete computers[i]
+                socket.broadcast.emit('getRoom', {data: computers })
+            }
+        } else { // Is mobile
+            socket.broadcast.to(socket.clientId).emit('mobileDisconnected')
+        }
+    })
+});
 
 
 server.listen(1337)
