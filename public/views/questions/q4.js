19f7a00d992b53f85db42c1dfa7ff1fda238f4b0
ESSOUBAI Nadia <nessoubai@g-a102b-prof.local>
2019-04-24 17:12:55 +0200

    add objet 4

--- public/views/questions/q4.js
+++ public/views/questions/q4.js
@@ -145,7 +145,7 @@ desktop_html =
     <div id="object1" class="object"></div>
     <div id="object2" class="object"></div>
     <div id="object3" class="object"></div>
-    <div id="object4" class="object"></div>
+    <canvas id="object4" class="object"></canvas>
     <div class="object object5">Objet 5</div>
     <div class="object object6">Objet 6</div>
  </div>
@@ -227,7 +227,7 @@ desktop_script = () => {
     function init() {
         container = document.getElementById('object1');
         camera = new THREE.PerspectiveCamera( 80, (window.innerWidth/3) / (window.innerHeight/2), 10, 2000 );
-        camera.position.z = 240;
+        camera.position.z = 350;
 
         
         // scene
@@ -336,7 +336,7 @@ desktop_script = () => {
     function initObj2() {
         containerObj2 = document.getElementById('object2');
         cameraObj2 = new THREE.PerspectiveCamera( 90, (window.innerWidth/3) / (window.innerHeight/2), 10, 2000 );
-        cameraObj2.position.z = 240;
+        cameraObj2.position.z = 350;
 
 
         // scene
@@ -513,6 +513,228 @@ desktop_script = () => {
     }
     /**************** FIN OBJECT3 3D ****************/
 
+    /**************** OBJECT4 3D ****************/
+    window.requestAnimFrame =
+    window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function(callback) {
+        window.setTimeout(callback, 1000 / 60);
+};
+
+var canvas = document.getElementById('object4');
+var ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth/3;
+canvas.height = window.innerHeight/2;
+
+var settings = {
+
+    'abstract': {
+
+        'emission_rate': 1200,
+        'min_life': 1,
+        'life_range': 1,
+        'min_angle': 0,
+        'angle_range': 360,
+        'min_speed': 10,
+        'speed_range': 100,
+        'min_size': 1,
+        'size_range': 1,
+        'gravity': {
+            x: 0,
+            y: 0
+        },
+        'min_position': {
+            x: -30,
+            y: -30
+        },
+        'position_range': {
+            x: 60,
+            y: 60
+        }
+    },
+
+    'blood': {
+
+        'emission_rate': 1000,
+        'min_life': 1,
+        'life_range': 1,
+        'min_angle': 85,
+        'angle_range': 10,
+        'min_speed': 10,
+        'speed_range': 100,
+        'min_size': 1,
+        'size_range': 1,
+        'gravity': {
+            x: 0,
+            y: 100
+        }
+    },
+};
+
+var Particle = function(x, y, angle, speed, life, size, start_colour, colour_step) {
+
+    /* the particle's position */
+    this.pos = {
+        x: x || 0,
+        y: y || 0
+    };
+
+    /* set specified or default values */
+    this.speed = speed || 5;
+    this.life = life || 1;
+    this.size = size || 3;
+    this.lived = 0;
+
+    /* the particle's velocity */
+    var radians = angle * Math.PI / 180;
+
+    this.vel = {
+
+        x: Math.cos(radians) * speed,
+        y: -Math.sin(radians) * speed
+    };
+
+    /* the particle's colour values */
+    this.colour = start_colour;
+    this.colour_step = colour_step;
+};
+
+var Emitter = function(x, y, settings) {
+
+    /* the emitter's position */
+    this.pos = {
+
+        x: x,
+        y: y
+    };
+
+    /* set specified values */
+    this.settings = settings;
+
+    /* How often the emitter needs to create a particle in milliseconds */
+    this.emission_delay = 1000 / settings.emission_rate;
+
+    /* we'll get to these later */
+    this.last_update = 0;
+    this.last_emission = 0;
+
+    /* the emitter's particle objects */
+    this.particles = [];
+
+    /* particle position settings */
+    this.position_vary = this.settings.position_range || false;
+    this.min_position = this.settings.min_position || {x: 0, y: 0};
+};
+
+Emitter.prototype.update = function() {
+
+    /* set the last_update variable to now if it's the first update */
+
+    if (!this.last_update) {
+
+        this.last_update = Date.now();
+
+        return;
+    }
+
+    /* get the current time */
+    var time = Date.now();
+
+    /* work out the milliseconds since the last update */
+    var dt = time - this.last_update;
+
+    /* add them to the milliseconds since the last particle emission */
+    this.last_emission += dt;
+
+    /* set last_update to now */
+    this.last_update = time;
+
+    /* check if we need to emit a new particle */
+    if (this.last_emission > this.emission_delay) {
+
+        /* find out how many particles we need to emit */
+        var i = Math.floor(this.last_emission / this.emission_delay);
+
+        /* subtract the appropriate amount of milliseconds from last_emission */
+        this.last_emission -= i * this.emission_delay;
+
+        while (i--) {
+
+            var life = this.settings.min_life + Math.random() * this.settings.life_range;
+
+            this.particles.push(
+                new Particle(
+                    this.min_position.x + (this.position_vary ? Math.random() * this.position_vary.x : 0),
+                    this.min_position.y + (this.position_vary ? Math.random() * this.position_vary.y : 0),
+                    this.settings.min_angle + Math.random() * this.settings.angle_range,
+                    this.settings.min_speed + Math.random() * this.settings.speed_range,
+                    life,
+                    this.settings.min_size + Math.random() * this.settings.size_range,
+                )
+            );
+        }
+    }
+
+    /* convert dt to seconds */
+    dt /= 1000;
+
+    /* loop through the existing particles */
+    var i = this.particles.length;
+
+    while (i--) {
+        var particle = this.particles[i];
+
+        /* skip if the particle is dead */
+        if (particle.dead) {
+            /* remove the particle from the array */
+            this.particles.splice(i, 1);
+            continue;
+        }
+
+        /* add the seconds passed to the particle's life */
+        particle.lived += dt;
+
+        /* check if the particle should be dead */
+        if (particle.lived >= particle.life) {
+            particle.dead = true;
+            continue;
+        }
+
+        /* calculate the particle's new position based on the forces multiplied by seconds passed */
+        particle.vel.x += this.settings.gravity.x * dt;
+        particle.vel.y += this.settings.gravity.y * dt;
+
+        particle.pos.x += particle.vel.x * dt;
+        particle.pos.y += particle.vel.y * dt;
+
+
+        ctx.fillStyle = '#b07230';
+
+        var x = this.pos.x + particle.pos.x;
+        var y = this.pos.y + particle.pos.y;
+
+        ctx.beginPath();
+        ctx.arc(x, y, particle.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
+};
+
+var emitter = new Emitter(canvas.width / 2, canvas.height / 2, settings.abstract);
+
+function loop() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    emitter.update();
+    requestAnimFrame(loop);
+}
+
+loop();
+    /**************** FIN OBJECT4 3D ****************/
+
+    /**************** OBJECT5 3D ****************/
 }
 
 desktop_transition = ["out", "in"]
