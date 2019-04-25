f0a558178a04bf9198a4dbbfa64fc2816d931858
ESSOUBAI Nadia <nessoubai@g-a102b-prof.local>
2019-04-25 16:13:57 +0200

    update background

---
+++ public/lib/vanta/background.js
@@ -0,0 +1,10 @@
+let background = VANTA.FOG({
+    el: "#background_anim",
+    highlightColor: 0xbbe9fd,
+    midtoneColor: 0xbed2fe,
+    lowlightColor: 0xfdeefd,
+    baseColor: 0xfae9fe,
+    blurFactor: 0.70,
+    speed: 2.70,
+    zoom: 2.20
+  })
--- public/views/questions/q6.js
+++ public/views/questions/q6.js
@@ -39,7 +39,11 @@
  desktop_html = 
  `
  <div class="text_center">
-    <h1>Ceci est la question 6</h1>
+    <h1>Quel est le moment de la journée le plus agréable visuellement ?</h1>
+    <button id="test1">Nuit</button>
+    <button id="test2">Lever du soleil/button>
+    <button id="test3">Petit matin</button>
+    <button id="test4">Journée</button>
 </div>
  `
  
@@ -52,7 +56,62 @@
  }]
  
  desktop_script = () => {
- 
+    let backgroundTest1;
+    let backgroundTest2;
+    let backgroundTest3;
+    let backgroundTest4;
+
+    document.getElementById("test1").onclick = function (){
+        background.destroy()
+        backgroundTest1 = VANTA.FOG({
+            el: "#background_anim",
+            highlightColor: 0x055ca4,
+            midtoneColor: 0x9198c7,
+            baseColor: 0x000041,
+            blurFactor: 0.70,
+            speed: 0.70,
+            zoom: 0.40
+          })
+    }
+    document.getElementById("test2").onclick = function (){
+        backgroundTest1.destroy()
+        backgroundTest2 = VANTA.FOG({
+            el: "#background_anim",
+            highlightColor: 0xc6c8f8,
+            midtoneColor: 0xf1a9b4,
+            lowlightColor: 0xf7d0d6,
+            baseColor: 0x9198c7,
+            blurFactor: 0.70,
+            speed: 0.70,
+            zoom: 0.40
+          })
+    }
+    document.getElementById("test3").onclick = function (){
+        backgroundTest2.destroy()
+        backgroundTest3 = VANTA.FOG({
+            el: "#background_anim",
+            highlightColor: 0xe6aaaf,
+            midtoneColor: 0xbee1f7,
+            lowlightColor: 0x606289,
+            baseColor: 0xffffff,
+            blurFactor: 0.70,
+            speed: 0.70,
+            zoom: 0.40
+          })
+    }
+    document.getElementById("test4").onclick = function (){
+        backgroundTest3.destroy()
+        backgroundTest4 = VANTA.FOG({
+            el: "#background_anim",
+            highlightColor: 0xffffff,
+            midtoneColor: 0xa5e0fa,
+            lowlightColor: 0xbadaf5,
+            baseColor: 0x94bfe0,
+            blurFactor: 0.70,
+            speed: 0.70,
+            zoom: 0.40
+          })
+    }
  }
  
  desktop_transition = ["out", "in"]
