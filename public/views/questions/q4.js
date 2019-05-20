caf30c61adf01444007d8e7e406ce708e71cded3
Nadia Essoubai <nadia.ess.94@hotmail.fr>
2019-05-20 13:45:48 +0200

    init q4

--- public/views/questions/q4.js
+++ public/views/questions/q4.js
@@ -139,303 +139,95 @@ desktop_script = () => {
     /******************* 
      ****** OBJET 1 ****
      ********************/
-    var container;
-    var camera, scene, renderer;
-    var mouseX = 0,
-        mouseY = 0;
-    var windowHalfX = window.innerWidth / 3;
-    var windowHalfY = window.innerHeight / 2;
-    var object;
-    init();
-    animate();
 
-    function init() {
-        container = document.getElementById('object1');
-        camera = new THREE.PerspectiveCamera(80, (window.innerWidth / 3) / (window.innerHeight / 2), 10, 2000);
-        camera.position.z = 350;
-        // scene
-        scene = new THREE.Scene();
-        var ambientLight = new THREE.AmbientLight(0xcccccc, 0.6);
-        scene.add(ambientLight);
-        var pointLight = new THREE.PointLight(0xffffff, 0.6);
-        camera.add(pointLight);
-        scene.add(camera);
-        // manager
-        function loadModel() {
-            object.position.y = -3;
-            scene.add(object);
-        }
-        var manager = new THREE.LoadingManager(loadModel);
-        manager.onProgress = function(item, loaded, total) {
-            console.log(item, loaded, total);
-        };
-        // model
-        function onProgress(xhr) {
-            if (xhr.lengthComputable) {
-                var percentComplete = xhr.loaded / xhr.total * 100;
-                console.log('model ' + Math.round(percentComplete, 2) + '% downloaded');
-            }
-        }
-        // MTL
-        var mtlLoader = new THREE.MTLLoader();
-        var url = "/both/assets/img/q4/bulles_eau_sans-transparence-ni-reflexion.mtl";
-        mtlLoader.load(url, function(materials) {
+     var scene = new THREE.Scene();
+     var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+     var container = document.getElementById('all_object');
 
-            materials.preload();
+     var renderer = new THREE.WebGLRenderer({
+        alpha: true
+    });
+     renderer.setSize(window.innerWidth, window.innerHeight);
+     container.appendChild(renderer.domElement);
 
-            var loader = new THREE.OBJLoader(manager);
-            loader.setMaterials(materials);
-            loader.load('/both/assets/img/q4/bulles_eau_2.obj', function(obj) {
+     var geometry = new THREE.BoxGeometry(1,1,1);
+     var material = new THREE.MeshBasicMaterial({color: 0x00ff00});
 
-                object = obj;
-
-            }, onProgress, onError);
-
-        });
-        //
-        function onError() {}
-        //
-        renderer = new THREE.WebGLRenderer({
-            alpha: true
-        });
-        renderer.setPixelRatio(window.devicePixelRatio);
-        renderer.setSize(window.innerWidth / 3, window.innerHeight / 2);
-        container.appendChild(renderer.domElement);
-        document.addEventListener('mousemove', onDocumentMouseMove, false);
-
-        window.addEventListener('resize', onWindowResize, false);
-    }
-    //
-    function onWindowResize() {
-        windowHalfX = window.innerWidth / 3;
-        windowHalfY = window.innerHeight / 2;
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-        renderer.setSize(450, 400);
-    }
-    //
-    function onDocumentMouseMove(event) {
-        mouseX = (event.clientX - windowHalfX) / 3;
-        mouseY = (event.clientY - windowHalfY) / 2;
-    }
-    //
-    function animate() {
-        requestAnimationFrame(animate);
-        render();
-    }
-    //
-    function render() {
-        camera.position.x += (mouseX - camera.position.x) * .05;
-        camera.position.y += (-mouseY - camera.position.y) * .05;
-        camera.position.y += (ball.position.y - camera.position.y) * .03;
-        camera.lookAt(scene.position);
-        renderer.render(scene, camera);
-    }
+     camera.position.z = 500;
+     camera.position.x = 220;
+     camera.position.y = -100;
 
 
-    /******************* 
-     ****** OBJET 2 ****
-     ********************/
-    var containerObj2;
-    var cameraObj2, sceneObj2, rendererObj2;
-    var mouseX = 0,
-        mouseY = 0;
-    var windowHalfX = window.innerWidth / 3;
-    var windowHalfY = window.innerHeight / 2;
-    var objectObj2;
-    initObj2();
-    animateObj2();
+     var controls = new THREE.OrbitControls(camera, renderer.domElement);
+     controls.enableDamping = true;
+     controls.campingFactor = 0.25;
+     controls.enableZoom = true;
 
-    function initObj2() {
-        containerObj2 = document.getElementById('object2');
-        cameraObj2 = new THREE.PerspectiveCamera(90, (window.innerWidth / 3) / (window.innerHeight / 2), 10, 2000);
-        cameraObj2.position.z = 350;
-        // scene
-        sceneObj2 = new THREE.Scene();
-        var ambientLightObj2 = new THREE.AmbientLight(0xcccccc, 0.6);
-        sceneObj2.add(ambientLightObj2);
-        var pointLightobj2 = new THREE.PointLight(0xffffff, 0.6);
-        cameraObj2.add(pointLightobj2);
-        sceneObj2.add(cameraObj2);
-        // manager
-        function loadModelObj2() {
-            objectObj2.position.y = -190;
-            objectObj2.position.x = -25;
-            sceneObj2.add(objectObj2);
-        }
-        var managerObj2 = new THREE.LoadingManager(loadModelObj2);
-        managerObj2.onProgress = function(item, loaded, total) {
-            console.log(item, loaded, total);
-        };
-        // model
-        function onProgress(xhr) {
-            if (xhr.lengthComputable) {
-                var percentComplete = xhr.loaded / xhr.total * 100;
-                console.log('model ' + Math.round(percentComplete, 2) + '% downloaded');
-            }
-        }
-        // MTL
-        var mtlLoaderObj2 = new THREE.MTLLoader();
-        var urlObj2 = "/both/assets/img/q4/feuilles.mtl";
-        mtlLoaderObj2.load(urlObj2, function(materialsObj2) {
+     var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 1.0);
+     keyLight.position.set(-100,0,100);
 
-            materialsObj2.preload();
+     var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
+     fillLight.position.set(100, 0, -100).normalize();
 
-            var loaderObj2 = new THREE.OBJLoader(managerObj2);
-            loaderObj2.setMaterials(materialsObj2);
-            loaderObj2.load('/both/assets/img/q4/feuilles.obj', function(obj) {
+     var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
+     backLight.position.set(100,0,-100).normalize();
 
-                objectObj2 = obj;
+     scene.add(keyLight);
+     scene.add(fillLight);
+     scene.add(backLight);
 
-            }, onProgress, onError);
-        });
-        //
-        function onError() {}
+     var mtlLoaderObj1 = new THREE.MTLLoader();
+     mtlLoaderObj1.setTexturePath('/both/assets/img/q4/');
+     mtlLoaderObj1.setPath('/both/assets/img/q4/');
+     mtlLoaderObj1.load('bulles_eau_2.mtl', function(materials) {
+         materials.preload();
 
-        rendererObj2 = new THREE.WebGLRenderer({
-            alpha: true
-        });
-        rendererObj2.setPixelRatio(window.devicePixelRatio);
-        rendererObj2.setSize(window.innerWidth / 3, window.innerHeight / 2);
-        containerObj2.appendChild(rendererObj2.domElement);
-        document.addEventListener('mousemove', onDocumentMouseMove, false);
+         var objLoaderObj1 = new THREE.OBJLoader();
+         objLoaderObj1.setMaterials(materials);
+         objLoaderObj1.setPath('/both/assets/img/q4/');
+         objLoaderObj1.load('bulles_eau_2.obj', function(object){
+             object.position.y = 60;
+             object.position.x = 0;
+             object.position.z = 0;
 
-        window.addEventListener('resize', onWindowResize, false);
-    }
-    //
-    function onWindowResize() {
-        windowHalfX = window.innerWidth / 3;
-        windowHalfY = window.innerHeight / 2;
-        cameraObj2.aspect = window.innerWidth / window.innerHeight;
-        cameraObj2.updateProjectionMatrix();
-        rendererObj2.setSize(300, 420);
-    }
-    //
-    function onDocumentMouseMove(event) {
-        mouseX = (event.clientX - windowHalfX) / 2;
-        mouseY = (event.clientY - windowHalfY) / 2;
-    }
-    //
-    function animateObj2() {
-        requestAnimationFrame(animateObj2);
-        renderObj2();
-    }
-    //
-    function renderObj2() {
-        cameraObj2.position.x += (mouseX - cameraObj2.position.x) * .05;
-        cameraObj2.position.y += (-mouseY - cameraObj2.position.y) * .05;
-        cameraObj2.position.y += (ball.position.y - cameraObj2.position.y) * .03;
-        cameraObj2.lookAt(sceneObj2.position);
-        rendererObj2.render(sceneObj2, cameraObj2);
-    }
+             scene.add(object);
+         })
+     })
 
+     var mtlLoaderObj4 = new THREE.MTLLoader();
+     mtlLoaderObj4.setTexturePath('/both/assets/img/q4/');
+     mtlLoaderObj4.setPath('/both/assets/img/q4/');
+     mtlLoaderObj4.load('montagnes_seules.mtl', function(materials) {
+         materials.preload();
 
+         var objLoaderObj4 = new THREE.OBJLoader();
+         objLoaderObj4.setMaterials(materials);
+         objLoaderObj4.setPath('/both/assets/img/q4/');
+         objLoaderObj4.load('montagnes_s300.obj', function(object){
+             object.position.y = -250;
+             object.position.x = -100;
+             object.position.z = 10;
 
-    /******************* 
-     ****** OBJET 3 ****
-     ********************/
-    var containerObj3;
-    var cameraObj3, sceneObj3, rendererObj3;
-    var mouseX = 0,
-        mouseY = 0;
-    var windowHalfX = window.innerWidth / 3;
-    var windowHalfY = window.innerHeight / 2;
-    var objectObj3;
-    initObj3();
-    animateObj3();
-    //
-    function initObj3() {
-        containerObj3 = document.getElementById('object3');
-        cameraObj3 = new THREE.PerspectiveCamera(80, (window.innerWidth / 3) / (window.innerHeight / 2), 10, 2000);
-        cameraObj3.position.z = 240;
-        // scene
-        sceneObj3 = new THREE.Scene();
-        var ambientLightObj3 = new THREE.AmbientLight(0xcccccc, 0.6);
-        sceneObj3.add(ambientLightObj3);
-        var pointLightobj3 = new THREE.PointLight(0xffffff, 0.6);
-        cameraObj3.add(pointLightobj3);
-        sceneObj3.add(cameraObj3);
-        // manager
-        function loadModelObj3() {
-            objectObj3.position.y = -3;
-            sceneObj3.add(objectObj3);
-        }
-        var managerObj3 = new THREE.LoadingManager(loadModelObj3);
-        managerObj3.onProgress = function(item, loaded, total) {
-            console.log(item, loaded, total);
-        };
-        // model
-        function onProgress(xhr) {
-            if (xhr.lengthComputable) {
-                var percentComplete = xhr.loaded / xhr.total * 100;
-                console.log('model ' + Math.round(percentComplete, 2) + '% downloaded');
-            }
-        }
-        // MTL
-        var mtlLoaderObj3 = new THREE.MTLLoader();
-        var urlObj3 = "/both/assets/img/q4/goutte_sans-transparence.mtl";
-        mtlLoaderObj3.load(urlObj3, function(materialsObj3) {
+             scene.add(object);
+         })
+     })
 
-            materialsObj3.preload();
+     var animate = function () {
+         requestAnimationFrame(animate);
 
-            var loaderObj3 = new THREE.OBJLoader(managerObj3);
-            loaderObj3.setMaterials(materialsObj3);
-            loaderObj3.load('/both/assets/img/q4/goutte.obj', function(obj) {
+         controls.update();
 
-                objectObj3 = obj;
+         renderer.render(scene, camera);
+     };
 
-            }, onProgress, onError);
-        });
-        //
-        function onError() {}
-        //
-        rendererObj3 = new THREE.WebGLRenderer({
-            alpha: true
-        });
-        rendererObj3.setPixelRatio(window.devicePixelRatio);
-        rendererObj3.setSize(window.innerWidth / 3, window.innerHeight / 2);
-        containerObj3.appendChild(rendererObj3.domElement);
-        document.addEventListener('mousemove', onDocumentMouseMove, false);
-        //
-        window.addEventListener('resize', onWindowResize, false);
-    }
-
-    function onWindowResize() {
-        windowHalfX = window.innerWidth / 3;
-        windowHalfY = window.innerHeight / 2;
-        cameraObj3.aspect = window.innerWidth / window.innerHeight;
-        cameraObj3.updateProjectionMatrix();
-        rendererObj3.setSize(450, 400);
-    }
-
-    function onDocumentMouseMove(event) {
-        mouseX = (event.clientX - windowHalfX) / 3;
-        mouseY = (event.clientY - windowHalfY) / 2;
-    }
-    //
-    function animateObj3() {
-        requestAnimationFrame(animateObj3);
-        renderObj3();
-    }
-
-    function renderObj3() {
-        cameraObj3.position.x += (mouseX - cameraObj3.position.x) * .05;
-        cameraObj3.position.y += (-mouseY - cameraObj3.position.y) * .05;
-        cameraObj3.position.y += (ball.position.y - cameraObj3.position.y) * .03;
-        cameraObj3.lookAt(sceneObj3.position);
-        rendererObj3.render(sceneObj3, cameraObj3);
-    }
-
-    /******************* 
-     ****** OBJET 4 ****
-     ********************/
+     animate();
 
 
 
     /**************** 
      *** TIMELINE ***
      ****************/
-    document.querySelector('.q4').style.fill = "#ffffff"
+    //document.querySelector('.q4').style.fill = "#ffffff"
 }
 
 desktop_transition = ["out", "in"]
