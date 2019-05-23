9df13b08e44a0a10c76c7b1ffba62cb8bdc8a8c0
RUCOJEVIC Rémi <rrucojevic@g-a102b-20.local>
2019-05-23 15:23:18 +0200

    input connexion

--- public/views/intro/connexion.js
+++ public/views/intro/connexion.js
@@ -15,7 +15,7 @@ mobile_html =
         <h1>Bienvenue sur Otium</h1>
         <p>Entrez votre code afin de connecter votre smartphone.
         </p>
-        <input class="btn" id="code" type="number">
+        <input class="btn" id="code" type="number" autocomplete="off" maxlength="6">
         <button class="start_q1">test connexion</button>
     </div>
     <!----------- 
