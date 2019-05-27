89a57e5480c8bb481729d9b49dd949a77414c284
Nadia Essoubai <nadia.ess.94@hotmail.fr>
2019-05-27 21:27:41 +0200

    add drap animation q5

--- public/views/questions/q5.js
+++ public/views/questions/q5.js
@@ -78,6 +78,7 @@ desktop_html =
     <div class="text_center">
         <h1 class="question_desktop">Choisissez un aspect</h1>
     </div>
+    <div id="drap" style="position: absolute; z-index:"></div>
  `
 
 desktop_listener1 = ["selector", "type", () => {
@@ -99,14 +100,91 @@ desktop_socketOn1 = ["q5_doigt", (data) => {
 }]
 
 desktop_script = () => {
-    let q5_actual_texture
-    let q5_textures = [
+    // let q5_actual_texture
+    // let q5_textures = [
 
-    ]
-    for (let i = 1; i <= 15; i++) {
-        q5_textures.push(new THREE.TextureLoader().load('/both/assets/textures/q5/texture_drap-'+i+'.jpg'))
+    // ]
+    // for (let i = 1; i <= 15; i++) {
+    //     q5_textures.push(new THREE.TextureLoader().load('/both/assets/textures/q5/texture_drap-'+i+'.jpg'))
+    // }
+
+
+    var container = document.getElementById('drap')
+
+	var vertexHeight = 15000,
+		planeDefinition = 100,
+		planeSize = 120000,
+		totalObjects = 1,
+		meshColor = "#005e97"; 
+
+	var camera = new THREE.PerspectiveCamera(55, window.innerWidth / window.innerHeight, 1, 400000)
+	camera.position.z = 11000;
+	camera.position.y = 11000;
+
+	var scene = new THREE.Scene();
+    
+
+	var texture = new THREE.TextureLoader().load( "/both/assets/textures/q5/texture_drap-7_NORMALES.jpg" );
+
+    var planeGeo = new THREE.PlaneGeometry(planeSize, planeSize, planeDefinition, planeDefinition);
+
+	var plane = new THREE.Mesh(planeGeo, new THREE.MeshBasicMaterial({
+        map: texture
+    }));
+    
+
+	plane.rotation.x -= Math.PI * .4;
+
+	scene.add(plane);
+
+	var renderer = new THREE.WebGLRenderer({
+        alpha: true
+    });
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+  container.appendChild(renderer.domElement);
+
+
+  updatePlane();
+
+	function updatePlane() {
+		for (var i = 0; i < planeGeo.vertices.length; i++) {
+      planeGeo.vertices[i].z += Math.random() * vertexHeight - vertexHeight;
+      planeGeo.vertices[i]._myZ = planeGeo.vertices[i].z
+		}
+	};
+
+	render();
+
+  var count = 0
+	function render() {
+		requestAnimationFrame(render);
+    // camera.position.z -= 150;
+    // var x = camera.position.x;
+    // var z = camera.position.z;
+    // camera.position.x = x * Math.cos(0.001) + z * Math.sin(0.001) - 10;
+    // camera.position.z = z * Math.cos(0.001) - x * Math.sin(0.001) - 10;
+    camera.lookAt(new THREE.Vector3(0, 5000, 0))
+
+    for (var i = 0; i < planeGeo.vertices.length; i++) {
+      var z = +planeGeo.vertices[i].z;
+      planeGeo.vertices[i].z = Math.sin(( i + count * 0.00003)) * (planeGeo.vertices[i]._myZ - (planeGeo.vertices[i]._myZ* 0.94))
+      plane.geometry.verticesNeedUpdate = true;
+
+      count += 0.15
     }
 
+		renderer.render(scene, camera);
+	}
+
+	window.addEventListener('resize', onWindowResize, false);
+
+	function onWindowResize() {
+		//changes the size of the canavs and updates it
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+		renderer.setSize(window.innerWidth, window.innerHeight);
+	}
 
 
     /**************** 
