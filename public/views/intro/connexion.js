3bbe93b555b800a87eebac50edd8234fc20bb7a5
Nadia Essoubai <nadia.ess.94@hotmail.fr>
2019-06-09 22:13:31 +0200

    add print form

--- public/views/intro/connexion.js
+++ public/views/intro/connexion.js
@@ -44,7 +44,7 @@ mobile_listener2 = [".start_q1", "click", () => {
     intro.connexion.transitionTo("mobile", intro.button_valider)
 }]
 
-mobile_listener3 = ["#code", "click", (e) => {
+mobile_listener3 = ["#code", "click", () => {
     document.querySelector(".firstMenu").style.display = "none"
 }]
 
@@ -55,6 +55,8 @@ let mobile_socketOn1 = ["mobileConnected", () => {
 
 // Script to be executed when the page is displayed
 mobile_script = () => {
+
+    //display menu
     document.querySelector(".firstMenu").style.display = "block"
     document.querySelector(".firstMenu2").style.display = "none"
     document.querySelector(".firstMenu3").style.display = "none"
@@ -62,21 +64,32 @@ mobile_script = () => {
     document.querySelector(".circle1").style.display = "none"
     document.querySelector(".circle2").style.display = "none"
 
-    
-    window.addEventListener('click', function(e){   
-        if (document.querySelector(".firstMenu1").contains(e.target)){
-            document.querySelector("#infoCredit").style.display = "block"
-            document.querySelector(".firstMenu1").style.display = "none"
-            document.querySelector("#navigation").style.opacity = "0"
-            document.querySelector("#buttons").style.opacity = "0"
-        } else{
-            document.querySelector(".firstMenu1").style.display = "block"
+   
+    // display credit and forms
+    var displayPageMenu;
+
+    document.querySelector(".firstMenu1").onclick = function() { 
+        document.querySelector("#infoCredit").style.display = "block"
+        document.querySelector("#navigation").style.opacity = "0"
+        document.querySelector("#buttons").style.opacity = "0"
+        displayPageMenu = "yes";
+    };
+
+    document.querySelector(".firstMenu3").onclick = function() { 
+        document.querySelector("#formulaire3D").style.display = "block"
+        document.querySelector("#navigation").style.opacity = "0"
+        document.querySelector("#buttons").style.opacity = "0"
+        displayPageMenu = "yes";
+    };
+
+    document.querySelector("#menu-toggler").onclick = function() { 
+        if (displayPageMenu === "yes"){
             document.querySelector("#infoCredit").style.display = "none"
+            document.querySelector("#formulaire3D").style.display = "none"
             document.querySelector("#navigation").style.opacity = "1"
             document.querySelector("#buttons").style.opacity = "1"
         }
-      });
-
+    };
 
 }
 
