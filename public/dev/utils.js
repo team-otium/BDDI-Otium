c82a81141dd4ed6ff049bc7cf86477cfd880cc0c
RemiRuc <remi.rucojevic@gmail.com>
2019-06-13 02:21:12 +0200

    q5work

--- public/dev/utils.js
+++ public/dev/utils.js
@@ -1,7 +1,7 @@
 let config = {
-    ip: "otium-project.herokuapp.com",
-    //ip: "10.137.26.140",
+    //ip: "otium-project.herokuapp.com",
+    ip: "192.168.1.51",
     //ip : "10.137.27.77",
-    port: 3000
+    port: 1337
 
 }
--- public/views/questions/q5.js
+++ public/views/questions/q5.js
@@ -55,8 +55,8 @@ mobile_listener3 = ["#q2_target", "touchend", (e) => {
 
 // Script to be executed when the page is displayed
 mobile_script = () => {
-    document.querySelector(".circle").style.display = "block"
-    document.querySelector(".circleIn").style.display = "block"
+    document.querySelector(".circle1").style.display = "block"
+    document.querySelector(".circle2").style.display = "block"
     ValidationBtn.canValidate = true
     ValidationBtn.actualPage = questions.q5
     ValidationBtn.nextPage = questions.q6
@@ -93,26 +93,26 @@ desktop_listener2 = ["selector", "type", () => {
 
 desktop_socketOn1 = ["q5", (data) => {
     if (data === "haut") {
-        q5_actual_texture++
-        if (q5_actual_texture > q5_textures.length - 1) q5_actual_texture = 0
+        window.q5_actual_texture++
+        if (window.q5_actual_texture > window.q5_textures.length - 1) window.q5_actual_texture = 0
     } else if (data === "bas") {
-        q5_actual_texture--
-        if (q5_actual_texture < 0) q5_actual_texture = q5_textures.length - 1
+        window.q5_actual_texture--
+        if (window.q5_actual_texture < 0) window.q5_actual_texture = window.q5_textures.length - 1
     }
-    scene.remove(plane);
+    window.sceneq5.remove(window.planeq5);
     createPlane()
 }]
 
 desktop_script = () => {
-    let q5_actual_texture = 0
-    let q5_textures = [
+    window.q5_actual_texture = 0
+    window.q5_textures = [
 
     ]
     for (let i = 1; i <= 15; i++) {
-        q5_textures.push(new THREE.TextureLoader().load('/both/assets/img/q5/texture_drap-'+i+'.jpg'))
+        window.q5_textures.push(new THREE.TextureLoader().load('/both/assets/img/q5/texture_drap-'+i+'.jpg'))
     }
 
-    var scene = new THREE.Scene();
+    window.sceneq5 = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
     var simplex = new SimplexNoise()
@@ -124,32 +124,32 @@ desktop_script = () => {
 
     var light = new THREE.PointLight(0xffffff, 1, 100);
     light.position.set(5, 5, 15);
-    scene.add(light);
+    window.sceneq5.add(light);
 
     var geometry, material, plane
-    function createPlane(){
-        geometry = new THREE.PlaneGeometry( 200, 200, 200, 200 );
-        material = new THREE.MeshLambertMaterial({ map: q5_textures[q5_actual_texture], side: THREE.DoubleSide, wireframe: false });
-        plane = new THREE.Mesh(geometry, material);
-        scene.add(plane);
-        plane.rotation.z = Math.PI / 6
-        plane.rotation.x = - Math.PI / 12
+    window.createPlane = ()=>{
+        window.geometryq5 = new THREE.PlaneGeometry( 200, 200, 200, 200 );
+        window.materialq5 = new THREE.MeshLambertMaterial({ map: q5_textures[window.q5_actual_texture], side: THREE.DoubleSide, wireframe: false });
+        window.planeq5 = new THREE.Mesh(window.geometryq5, window.materialq5);
+        window.sceneq5.add(window.planeq5);
+        window.planeq5.rotation.z = Math.PI / 6
+        window.planeq5.rotation.x = - Math.PI / 12
       }
 
-      createPlane()
+      window.createPlane()
 
     camera.position.z = 5;
 
     var animate = function () {
         requestAnimationFrame(animate);
 
-        for (var i = 0; i < geometry.vertices.length; i++) {
+        for (var i = 0; i < window.geometryq5.vertices.length; i++) {
             var z = (i + Date.now() * 1/100000)
-            geometry.vertices[i].z = simplex.noise4D(z,z,z,z) * 2
-            plane.geometry.verticesNeedUpdate = true;
+            window.geometryq5.vertices[i].z = simplex.noise4D(z,z,z,z) * 2
+            window.planeq5.geometry.verticesNeedUpdate = true;
           }
 
-        renderer.render(scene, camera);
+        renderer.render(window.sceneq5, camera);
     };
 
     animate();
