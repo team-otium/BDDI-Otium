db4541d620164a7d905e63e9c36f4b17e23abf95
RemiRuc <remi.rucojevic@gmail.com>
2019-06-17 15:11:02 +0200

    up

--- public/dev/utils.js
+++ public/dev/utils.js
@@ -1,12 +1,7 @@
 let config = {
     //ip: "otium-project.herokuapp.com",
-<<<<<<< HEAD
     ip: "192.168.43.103",
-=======
-    //ip: "192.168.43.173",
-    ip: "10.137.32.7",
->>>>>>> d50b4396d0aa8d32ce9ee0138163ff8fc015cb9f
     //ip : "10.137.27.77",
     port: 1337
-
+ 
 }
