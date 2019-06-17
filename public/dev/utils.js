4278c63d3791b4dd47c471a05faafb4f58899ecc
Nadia Essoubai <nadia.ess.94@hotmail.fr>
2019-06-17 10:46:11 +0200

    update contour anim

--- public/dev/utils.js
+++ public/dev/utils.js
@@ -1,6 +1,7 @@
 let config = {
     //ip: "otium-project.herokuapp.com",
-    ip: "192.168.43.173",
+    //ip: "192.168.43.173",
+    ip: "10.137.32.7",
     //ip : "10.137.27.77",
     port: 1337
 
