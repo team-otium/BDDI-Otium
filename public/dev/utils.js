a88e73d94313f4ac1860ee6648e6dac69d6d1a25
Nadia Essoubai <nadia.ess.94@hotmail.fr>
2019-06-17 16:42:41 +0200

    update cursor

--- public/dev/utils.js
+++ public/dev/utils.js
@@ -1,7 +1,7 @@
 let config = {
     //ip: "otium-project.herokuapp.com",
-    ip: "192.168.43.103",
-    //ip : "10.137.27.77",
+    //ip: "192.168.43.103",
+    ip : "10.137.32.7",
     port: 1337
  
 }
