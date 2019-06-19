8d5f9026b7bd6af013ead4369aa5a3e42f736bbf
Nadia Essoubai <nadia.ess.94@hotmail.fr>
2019-06-20 01:01:16 +0200

    move joystick

--- public/views/univers/univers.js
+++ public/views/univers/univers.js
@@ -58,29 +58,73 @@ mobile_script = () => {
     window.univJoy = false
 
 
-    if (!window.requestAnimationFrame) {
-        window.requestAnimationFrame = (function () {
-            return window.webkitRequestAnimationFrame ||
-                window.mozRequestAnimationFrame ||
-                window.oRequestAnimationFrame ||
-                window.msRequestAnimationFrame ||
-                function ( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
-                    window.setTimeout(callback, 1000 / 60);
-                };
-        })();
+    // if (!window.requestAnimationFrame) {
+    //     window.requestAnimationFrame = (function () {
+    //         return window.webkitRequestAnimationFrame ||
+    //             window.mozRequestAnimationFrame ||
+    //             window.oRequestAnimationFrame ||
+    //             window.msRequestAnimationFrame ||
+    //             function ( /* function FrameRequestCallback */ callback, /* DOMElement Element */ element) {
+    //                 window.setTimeout(callback, 1000 / 60);
+    //             };
+    //     })();
+    // }
+
+    // if (window.DeviceOrientationEvent) {
+    //     window.addEventListener("deviceorientation", function (eventData) {
+    //         var tiltLR = eventData.gamma;
+    //         var tiltFB = eventData.beta;
+    //         var dir = eventData.alpha;
+
+    //         socket.emit("univers", { tiltFB: eventData.beta, tiltLR: eventData.gamma, dir: eventData.alpha });
+    //     })
+    // } else {
+    //     alert("Sorry, your browser doesn't support Device Orientation");
+    // };
+
+    function startDrag(e) {
+      this.ontouchmove = this.onmspointermove = moveDrag;
+    
+      this.ontouchend = this.onmspointerup = function () {
+        this.ontouchmove = this.onmspointermove = null;
+        this.ontouchend = this.onmspointerup = null;
+      }
+    
+      var pos = [this.offsetLeft, this.offsetTop];
+      var that = this;
+      var origin = getCoors(e);
+    
+      function moveDrag(e) {
+        var currentPos = getCoors(e);
+        var deltaX = currentPos[0] - origin[0];
+        var deltaY = currentPos[1] - origin[1];
+        this.style.left = (pos[0] + deltaX) + 'px';
+        this.style.top = (pos[1] + deltaY) + 'px';
+        return false; // cancels scrolling
+      }
+    
+      function getCoors(e) {
+        var coors = [];
+        if (e.targetTouches && e.targetTouches.length) {
+          var thisTouch = e.targetTouches[0];
+          coors[0] = thisTouch.clientX;
+          coors[1] = thisTouch.clientY;
+        } else {
+          coors[0] = e.clientX;
+          coors[1] = e.clientY;
+        }
+        return coors;
+      }
     }
-
-    if (window.DeviceOrientationEvent) {
-        window.addEventListener("deviceorientation", function (eventData) {
-            var tiltLR = eventData.gamma;
-            var tiltFB = eventData.beta;
-            var dir = eventData.alpha;
-
-            socket.emit("univers", { tiltFB: eventData.beta, tiltLR: eventData.gamma, dir: eventData.alpha });
-        })
-    } else {
-        alert("Sorry, your browser doesn't support Device Orientation");
-    };
+    
+    var elements = document.querySelectorAll('#univ_yot_circle');
+    [].forEach.call(elements, function (element) {
+      element.ontouchstart = element.onmspointerdown = startDrag;
+    });
+    
+    document.ongesturechange = function () {
+      return false;
+    }
 }
 
 // Name of the transitions classes [when he leave, when he arrive]
